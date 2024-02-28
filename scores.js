1ef1faf272b2f13cdfef692e30caef93281ef80c
cheewhoo <cheewhoo@byu.edu>
2024-02-28 15:19:13 -0700

    corrected weather temperature to be a placeholder for where my site will call on another service

--- scores.js
+++ scores.js
@@ -4,4 +4,9 @@ document.addEventListener('DOMContentLoaded', function() {
     let losses = localStorage.getItem(`${playerName}_losses`) || 0;
     document.getElementById('wins').textContent = `Wins: ${wins}`;
     document.getElementById('losses').textContent = `Losses: ${losses}`;
+    let randomWeather = getRandomNumber(0, 100);
+    document.getElementById('weather').textContent = `Temperature: ${randomWeather} F`;
 });
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
